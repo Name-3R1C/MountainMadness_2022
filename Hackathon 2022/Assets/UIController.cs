81db212dbf4145ed0b8b93b0fb5e837b4af25758
KTZHANG <kennyz620@hotmail.com>
2022-04-29 22:05:37 -0700

    Added maze

--- Hackathon 2022/Assets/UIController.cs
+++ Hackathon 2022/Assets/UIController.cs
@@ -6,6 +6,8 @@ public class UIController : MonoBehaviour
 {
     List<int> courses;
 
+    int[] fullcrslist = { 120, 125, 225, 276, 300, 150, 152, 232, 316, 340, 103, 105, 201, 325, 428, 101, 110, 102, 105, 110 };
+    GameObject[] coursesList;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,9 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       foreach (int course in fullcrslist)
+        {
+
+        }
     }
 }
